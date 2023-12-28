e802eb9e246663a18c11484f08720022f28e3ba9
liuxin <liuxin85@gmail.com>
2023-12-28 19:58:52 +0800

    nest authentication-session

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // POST /login
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return { msg: 'Logged in!' };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
+  getHello(@Request() req): string {
+    return req.user;
+  }
+}
